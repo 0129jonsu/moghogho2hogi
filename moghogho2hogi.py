958aa961731a9bd6a6bdcc2a5e2cb43ea8576a0d
0129jonsu <85074308+0129jonsu@users.noreply.github.com>
2021-10-26 17:19:12 +0900

    Update moghogho2hogi.py

--- moghogho2hogi.py
+++ moghogho2hogi.py
@@ -302,8 +302,8 @@ async def on_message(message):
         각인1 = ['◇','◇','◇','◇','◇','◇','◇','◇','◇','◇']
         각인2 = ['◇','◇','◇','◇','◇','◇','◇','◇','◇','◇']
         감소 = ['◇','◇','◇','◇','◇','◇','◇','◇','◇','◇']
-       
-        msg = await message.channel.send(f"★돌 시뮬★ <@{add_user}>(이)가 깎는중!\n각인1☝️  : {각인1} \n 각인2✌️ : {각인2} \n 감소 👎  : {감소} \n 확률 : 75%")
+        
+        msg = await message.channel.send(f'★돌 시뮬★ <@{add_user}>(이)가 깎는중!\n각인1☝️  : {각인1} \n 각인2✌️ : {각인2} \n 감소 👎  : {감소} \n 확률 : 75%')
         stone_dic[add_user] = stone_data()
         stone_dic[add_user].set_stone_msg(msg)
         await stone_dic[add_user].stone_msg.add_reaction('☝️')
