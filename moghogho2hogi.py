63484489266f2d7107bd0272bb314d6e8a523c73
0129jonsu <85074308+0129jonsu@users.noreply.github.com>
2021-10-26 12:35:16 +0900

    Update moghogho2hogi.py

--- moghogho2hogi.py
+++ moghogho2hogi.py
@@ -316,10 +316,6 @@ async def on_message(message):
         await stone_dic[add_user].stone_msg.add_reaction('✌️')
         await stone_dic[add_user].stone_msg.add_reaction('👎')
         
-        
-access_token = os.environ['BOT_TOKEN']
-client.run(access_token)
-
 #명령어 리스트 출력
     if message.content.startswith('명령어!'):
         await message.channel.send('명령어 list : 돌깎자!, 뭐먹, 대답, 2호기, 오늘도, 니얼굴, 야, ㅋㅋㅋㅋ, 가위바위보 ?, 강화!, 토끼, $')
@@ -402,4 +398,5 @@ async def on_raw_reaction_remove(payload):
     except KeyError as e:
             pass        
 
-
+access_token = os.environ['BOT_TOKEN']
+client.run(access_token)
