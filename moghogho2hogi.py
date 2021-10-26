38e0c441f6374a702163614c22685dc952c30c17
0129jonsu <85074308+0129jonsu@users.noreply.github.com>
2021-10-26 17:21:21 +0900

    Update moghogho2hogi.py

--- moghogho2hogi.py
+++ moghogho2hogi.py
@@ -386,5 +386,5 @@ async def on_raw_reaction_remove(payload):
     except KeyError as e:
             pass 
 
-access_token = os.environ['BOT_TOKEN2]
+access_token = os.environ['BOT_TOKEN]
 client.run(access_token)
